2adc20c99cb098db450dbadbb674851bf2d98d4a
hafidz0098 <hafidznak123@gmail.com>
2023-06-15 20:41:21 +0800

    add homepage

---
+++ layouts/default.js
@@ -0,0 +1,13 @@
+//import component Navbar
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}

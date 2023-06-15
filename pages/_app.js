d749f004e30ba7250877f61a47152ae4a5ff6ef2
hafidz0098 <hafidznak123@gmail.com>
2023-06-15 20:30:17 +0800

    install dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'remixicon/fonts/remixicon.css'
+
+//import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Head from 'next/head';
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp

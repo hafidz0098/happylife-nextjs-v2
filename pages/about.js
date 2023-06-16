16fd5907aec31a770cb8943559b87e8cadbb4997
hafidz0098 <hafidznak123@gmail.com>
2023-06-16 23:02:18 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,7 @@ function About(){
                 Meet Our Team
             </div>
           <div className="row">
-            <div className="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+            <div className="col-lg-6">
               <div className="member d-flex align-items-start">
                 <div className="pic"><img src="/Group 107.png" className="img-fluid" alt=""/></div>
                 <div className="member-info">
@@ -35,31 +35,43 @@ function About(){
               </div>
             </div>
  
-            <div className="col-lg-6 mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="200">
+            <div className="col-lg-6 mt-4 mt-lg-0">
               <div className="member d-flex align-items-start">
                 <div className="pic"><img src="/Group 107.png" className="img-fluid" alt=""/></div>
                 <div className="member-info">
                   <h4>Septian Aji Saputra</h4>
                   <span>Frontend Developer</span>
                   <div className="social">
-                    <a href="https://github.com/septiannaji"><i className="ri-github-fill"></i></a>
-                    <a href="https://www.instagram.com/septiannaji/"><i className="ri-instagram-fill"></i></a>
-                    <a href="https://www.linkedin.com/in/septian-aji-saputra-aa0097276/"> <i className="ri-linkedin-box-fill"></i> </a>
+                    <Link href="https://github.com/septiannaji" target="_blank">
+                      <i className="ri-github-fill"></i>
+                    </Link>
+                    <Link href="https://www.instagram.com/septiannaji/" target="_blank">
+                      <i className="ri-instagram-fill"></i>
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/septian-aji-saputra-aa0097276/" target="_blank">
+                      <i className="ri-linkedin-box-fill"></i>
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
  
-            <div className="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="300">
+            <div className="col-lg-6 mt-4">
               <div className="member d-flex align-items-start">
                 <div className="pic"><img src="/Group 107.png" className="img-fluid" alt=""/></div>
                 <div className="member-info">
                   <h4>Aulia Azzaskia</h4>
                   <span>Frontend Developer</span>
                   <div className="social">
-                    <a href=""><i className="ri-github-fill"></i></a>
-                    <a href=""><i className="ri-instagram-fill"></i></a>
-                    <a href=""> <i className="ri-linkedin-box-fill"></i> </a>
+                    <Link href="#" target="_blank">
+                      <i className="ri-github-fill"></i>
+                    </Link>
+                    <Link href="#" target="_blank">
+                      <i className="ri-instagram-fill"></i>
+                    </Link>
+                    <Link href="#" target="_blank">
+                      <i className="ri-linkedin-box-fill"></i>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -72,9 +84,15 @@ function About(){
                   <h4>Indy Istiqamah</h4>
                   <span>Frontend Developer</span>
                   <div className="social">
-                    <a href="https://github.com/inndyis"><i className="ri-github-fill"></i></a>
-                    <a href="https://www.instagram.com/inndyis/"><i className="ri-instagram-fill"></i></a>
-                    <a href="https://www.linkedin.com/in/indy-istiqamah/"> <i className="ri-linkedin-box-fill"></i> </a>
+                    <Link href="https://github.com/inndyis" target="_blank">
+                      <i className="ri-github-fill"></i>
+                    </Link>
+                    <Link href="https://www.instagram.com/inndyis/" target="_blank">
+                      <i className="ri-instagram-fill"></i>
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/indy-istiqamah/" target="_blank">
+                      <i className="ri-linkedin-box-fill"></i>
+                    </Link>
                   </div>
                 </div>
               </div>
